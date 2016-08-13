1ea1c95d1958592c8e0a1e3c604a72b0178c7c3d
Lucas Henrique <luketevl@gmail.com>
2016-08-13 03:40:59 -0300

    fix sum.hs

--- sum2numbers/sum.hs
+++ sum2numbers/sum.hs
@@ -1,7 +1,13 @@
-import Data.List
+module Main where
 import System.IO
 
--- You cant define the types of your function
--- my_sum :: Double -> Double -> Double
-
 my_sum num1 num2 = num1 + num2
+main = do
+    putStrLn "Insert the first number"
+    n1 <- readLn
+    putStrLn "Insert the second number"
+    n2 <- readLn
+    putStrLn $ "Result "
+    let result = my_sum n1 n2
+    print(result)
+    putStrLn $ " ;) "
